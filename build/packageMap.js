44e1ffb548bb590c493df801bf116b6f80bf79a7
Nowilus <nowakowskilukasznl@gmail.com>
2022-05-13 16:32:30 +0200

    rest of files

---
+++ build/packageMap.js
@@ -0,0 +1,8 @@
+require({
+    packages: [
+      {
+        name: 'react_module_bundle',
+        location: com_ibm_bpm_coach.getManagedAssetUrl('react_module_bundle.zip', com_ibm_bpm_coach.assetType_WEB)
+      }
+    ]
+  });
